944e940881b3360136e566aef12aa9e2546388a0
merlinrom <bgr.romina@gmail.com>
2021-05-27 11:45:26 +0430

    file jadid

--- ui.py
+++ ui.py
@@ -1,1 +1,24 @@
-print("hello word")
+
+from tkinter import*
+from tkinter.font import families
+def add():
+    print("hello")
+root = Tk()
+l1 = Label(root, text="name:")
+l1.grid(row=0, column=0)
+var1 = StringVar()
+e1 = Entry(root,textvariable=var1)
+e1.grid(row=0, column=1)
+l2 = Label(root, text="family:")
+l2.grid(row=1, column=0)
+var2 = StringVar()
+e2 = Entry(root, textvariable=var2)
+e2.grid(row=1, column=1)
+l3 = Label(root, text="age:")
+l3.grid(row=2, column=0)
+var3 = StringVar()
+e3 = Entry(root,textvariable=var3)
+e3.grid(row=2, column=1)
+b1 = Button(root, text="Register",command=add)
+b1.grid(row=3, column=0, columnspan=2, sticky='we')
+root.mainloop()
